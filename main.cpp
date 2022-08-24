e2da0ddc6e1ac88f92e4ba8d9ae7f6fac3aa43d6
Hxy <huxiaoyang1208@qq.com>
2022-08-24 09:23:50 +0800

    Default Changelist

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define ra register int
 using namespace std;
+const int maxn=1e5+5;
+int n,a[maxn];
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    cout<<"1";
-    for(int i(1);i<=10;++i){
-
+    scanf("%d",&n);
+    for(ra i(1);i<=n;++i){
+        scanf("%d",&a+i);
+    }
+    int cnt=(n+1)/2;
+    for(ra i(1);i<=cnt;i+=2){
+        
     }
     return 0;
 }
